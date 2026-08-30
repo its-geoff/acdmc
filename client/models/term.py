cf63eeb6d0fc617d1c3b1ace22f31a9646ca9f3f
its-geoff <109061107+its-geoff@users.noreply.github.com>
2026-08-30 01:30:14 -0700

    added missing variable to from_row docstring

--- client/models/term.py
+++ client/models/term.py
@@ -164,6 +164,7 @@ class Term:
 
         Args:
             id: The ID of the Term to construct.
+            title: The title of the Term.
             start_date: The start date of the Term.
             end_date: The end date of the Term.
             active: Whether the Term is currently active.
